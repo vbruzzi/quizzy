f3361c87d8bb095151fbb41765e777d7d4de961c
Vitor-Bruzzi <vitor.wcb@gmail.com>
2019-07-22 21:22:36 -0600

    Fixed random quiz

--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -32,16 +32,16 @@ export class QuizComponent implements OnInit {
     private service: QuizesService,
     private route: ActivatedRoute,
     ) {}
-    
+
   // Stores all questions to pass on to quizview
-  questions: Object;
-  quizName:string;
+  questions: object;
+  quizName: string;
 
   quizLoaded = false;
   // Answers to pass to quizresults
-  answers: Array<Object>;
+  answers: Array<object>;
   // Status for showing quiz/results (true means you're still doing quiz)
-  status: boolean = true;
+  status = true;
   // Id for quiz
   quizUrl: string;
 
@@ -51,30 +51,40 @@ export class QuizComponent implements OnInit {
     this.quizLoaded = false;
   }
 
-  uploadAnswers(result){
+  uploadAnswers(result) {
     this.service.uploadAnswers(result, this.quizUrl);
   }
 
   endQuiz() {
-    if(this.answers){
+    if(this.answers) {
       this.status = false;
     }
   }
 
   formReady() {
-    if(this.questions){
+    if(this.questions) {
       this.quizLoaded = true;
     }
+  }
+
+  mapData(data) {
+    this.quizName = data['name'];
+    this.questions = data['questions'];
+    this.quizUrl = data['id'];
   }
 
   ngOnInit() {
     // Gets quiz ID from url
     const id = this.route.snapshot.params['id'];
     // Fetches quiz via API
-    this.service.getQuiz(id).subscribe(data => {
-      this.quizName = data['name'];
-      this.questions = data['questions'];
-      this.quizUrl = data['id'];
-    });
+    if(id === 'random') {
+      this.service.getRandom().subscribe(data => {
+        this.mapData(data);
+      });
+    } else {
+      this.service.getQuiz(id).subscribe(data => {
+        this.mapData(data);
+      });
+    }
   }
 }
